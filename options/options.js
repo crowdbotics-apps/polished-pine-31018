2f82cded1fb1e9275a5c0198a558ea32837a66b9
Crowdbotics <team@crowdbotics.com>
2021-10-01 13:53:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "polished_pine_31018",
+  url: "https://polished_pine_31018.botics.co",
+  api: "https://polished_pine_31018.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
